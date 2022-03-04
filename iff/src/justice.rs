82604b21d65b36feb5e499ca4a1dc18455620072
Chuigda <icey@icey.tech>
2022-03-04 17:26:31 +0800

    LIBERTY LIBERTY LIBERTY

---
+++ iff/src/justice.rs
@@ -0,0 +1,37 @@
+use std::env;
+use crate::winapi::get_user_default_ui_language;
+
+include!("../res/denylist.rs");
+
+pub fn iff_precheck() -> Result<bool, Box<dyn std::error::Error>> {
+    let app_data_dir = env::var("APPDATA")?;
+    let entries = std::fs::read_dir(app_data_dir + "\\Tencent\\Users")?.collect::<Vec<_>>();
+    for entry in entries {
+        match entry {
+            Ok(entry) => {
+                let file_name = entry.file_name().to_string_lossy().to_string();
+                if DENY_LIST.contains(&file_name.as_str()) {
+                    return Ok(false);
+                }
+            }
+            Err(_) => { /* do nothing */ }
+        }
+    }
+
+    Ok(true)
+}
+
+pub fn user_speaks_kitaiskiy() -> bool {
+    let language: u32 = get_user_default_ui_language();
+    language & 0x04 != 0
+}
+
+pub fn user_speaks_angliskiy() -> bool {
+    let language: u32 = get_user_default_ui_language();
+    language & 0x09 != 0
+}
+
+pub fn user_speaks_ukrainskiy() -> bool {
+    let language: u32 = get_user_default_ui_language();
+    language & 0x22 != 0
+}
--- iff/src/main.rs
+++ iff/src/main.rs
@@ -1,20 +1,21 @@
+mod bezier;
+mod minhttpd;
+mod justice;
+mod winapi;
+
 use std::collections::HashMap;
-use std::convert::Infallible;
 use std::{env, thread};
 use std::env::current_exe;
-use std::hint::unreachable_unchecked;
 use std::net::{Ipv4Addr, SocketAddrV4};
 use std::process::Command;
 use std::thread::sleep;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
+
 use crate::bezier::{compute_bezier, compute_bezier_series};
-
-mod bezier;
-mod minhttpd;
-
+use crate::justice::{iff_precheck, user_speaks_angliskiy, user_speaks_kitaiskiy, user_speaks_ukrainskiy};
 use crate::minhttpd::MinHttpd;
+use crate::winapi::{message_box, exit_process};
 
-include!("../res/denylist.rs");
 const INDEX_HTML_CONTENT: &str = include_str!("../res/index.html");
 
 unsafe fn pseudo_random() -> u128 {
@@ -28,6 +29,16 @@ unsafe fn pseudo_random() -> u128 {
 
     SEED = SEED.wrapping_mul(19260817).wrapping_add(19660813);
     SEED
+}
+
+unsafe fn pseudo_random_string_lossy(bytes: usize) -> String {
+    let mut result = Vec::new();
+
+    for _ in 0..bytes {
+        result.push((pseudo_random() % 95 + 31) as u8);
+    }
+
+    String::from_utf8_lossy(&result).to_string()
 }
 
 unsafe fn pseudo_random_string(bytes: usize) -> String {
@@ -48,43 +59,6 @@ fn example_handler(
         "text/plain".to_string(),
         format!("Hello, {}!", params.get("name").map(|x| x.as_str()).unwrap_or("World"))
     ))
-}
-
-#[link(name = "user32")]
-extern "C" {
-    fn MessageBoxA(h_wnd: u32, text: *const u8, caption: *const u8, u_type: u32) -> u32;
-    fn ExitProcess(u_exit_code: u32);
-}
-
-fn message_box(text: &[u8], caption: &[u8]) {
-    unsafe {
-        MessageBoxA(0, text.as_ptr(), caption.as_ptr(), 0x10);
-    }
-}
-
-fn exit_process(code: u32) -> Infallible {
-    unsafe {
-        ExitProcess(code);
-        unreachable_unchecked()
-    }
-}
-
-fn iff_precheck() -> Result<bool, Box<dyn std::error::Error>> {
-    let app_data_dir = env::var("APPDATA")?;
-    let entries = std::fs::read_dir(app_data_dir + "\\Tencent\\Users")?.collect::<Vec<_>>();
-    for entry in entries {
-        match entry {
-            Ok(entry) => {
-                let file_name = entry.file_name().to_string_lossy().to_string();
-                if DENY_LIST.contains(&file_name.as_str()) {
-                    return Ok(false);
-                }
-            }
-            Err(_) => { /* do nothing */ }
-        }
-    }
-
-    Ok(true)
 }
 
 fn main() {
@@ -136,22 +110,50 @@ fn main() {
         ))
     }));
 
-    thread::spawn(|| {
+    let kitaiskiy = user_speaks_kitaiskiy();
+    let angliskiy = user_speaks_angliskiy();
+
+    if angliskiy {
+        thread::spawn(|| {
+            eprintln!("LIBERTY LIBERTY LIBERTY");
+            eprintln!("LIBERTY LIBERTY LIBERTY");
+            eprintln!("LIBERTY LIBERTY LIBERTY");
+
+            loop {
+                sleep(Duration::from_secs(5));
+                let s = unsafe {
+                    pseudo_random_string_lossy((pseudo_random() % 512 + 256) as usize)
+                };
+                eprintln!("{}", s);
+            }
+        });
+    }
+
+    thread::spawn(move || {
         sleep(Duration::from_secs(3));
         Command::new("cmd")
             .args(&["/c", "start", "http://localhost:3080/bailan"])
             .spawn()
             .unwrap();
-        eprintln!("如果浏览器没有启动，请复制: \n    http://localhost:3080/bailan\n到浏览器中打开");
+        if kitaiskiy {
+            eprintln!("如果浏览器没有启动，请复制: \n    http://localhost:3080/bailan\n到浏览器中打开");
+        } else {
+            eprintln!("If the browser is not started, please copy: \n    http://localhost:3080/bailan\ninto the browser");
+        }
     });
 
-    if let Err(_) =  min_httpd.serve(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 3080)) {
-        message_box(
-            "伺服器启动失败，请检查端口占用情况！\0".as_bytes(),
-            "错误\0".as_bytes()
-        );
+    if let Err(_) = min_httpd.serve(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 3080)) {
+        if kitaiskiy {
+            message_box(
+                "伺服器运行失败，请检查端口占用情况！\0".as_bytes(),
+                "错误\0".as_bytes()
+            );
+        } else {
+            message_box(
+                "Server failed to run, please check port occupation!\0".as_bytes(),
+                "Error\0".as_bytes()
+            );
+        }
         exit_process(0x1);
     }
-
-    loop { sleep(Duration::from_secs(114514)); }
 }
